e217a8ea5e4296b45c3d0eb2f6213a2392c8022d
Joao0028 <joaopaulonascimento720@gmail.com>
2023-12-07 05:26:35 -0300

    Aprendendo a usar o File System para ler um arquivo HTML

--- Arquivos do curso de Node/Seção3/2_retornando_html/index.js
+++ Arquivos do curso de Node/Seção3/2_retornando_html/index.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req, res ) => {
     res.statusCode = 200 // Significa que requisição foi bem sucedida
     res.setHeader('Contenty-Type', 'text/html') // Estou dizendo para o node que o tipo de conteúdo é html, e partir disso o programa esta habito para enviar html
-    res.end('<h1>Olá, este é meu primeiro server com HTML!</h1>')
+    res.end('<h1>Olá, este é meu primeiro server com HTML!</h1><p>Testando atuualização</p>')
 })
 
 server.listen(port, () => { 
