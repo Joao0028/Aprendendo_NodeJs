856e0d9c0b8ea35da34bd04346693b5a678b2339
Joao0028 <joaopaulonascimento720@gmail.com>
2023-12-07 06:16:31 -0300

    Aprendendo a atualizar um arquivo com o appendFile

--- Arquivos do curso de Node/Seção3/7_atualizando_arquivos/index.js
+++ Arquivos do curso de Node/Seção3/7_atualizando_arquivos/index.js
@@ -15,7 +15,10 @@ const server = http.createServer((req, res ) => {
             return res.end()
         })
     } else {
-        fs.writeFile("arquivo.txt", name, function(err, data){
+
+        const nameNewLine = name + "\r\n" // O "\r\n" serve para quebrar linha no windows e linux
+
+        fs.appendFile("arquivo.txt", nameNewLine, function(err, data){ // Adicionei o appendFile para atualizar arquivo.txt ao invés de apagar o antigo e adiconar um com novo nome
             res.writeHead(302,{ // 302 é o status de redirecionamento ou redirect
                 location: "/", // Ao enviar o name, seremos redirecionados novamente a página padrão que fica no path "/", pórem os dados serão apagados para adicionar novamente. E será criado um arquivo.txt com o name que passamos no input
             }) 
